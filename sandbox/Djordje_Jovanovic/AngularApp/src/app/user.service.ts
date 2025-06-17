cbcc70d75b11896283473da32ca98e6f3600de20
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-06-17 17:21:04 +0200

    Izmenjen Angular deo

--- sandbox/Djordje_Jovanovic/AngularApp/src/app/user.service.ts
+++ sandbox/Djordje_Jovanovic/AngularApp/src/app/user.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user.model';
 import { Observable } from 'rxjs';
+import { environment } from '../environment/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private apiUrl = 'https://localhost:7277/api/Usermaster';
+  private apiUrl = `${environment.apiBaseUrl}`;
 
   constructor(private http: HttpClient) {}
 
