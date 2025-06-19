c44d62fe0f1b8a51453469de94c7524ee809e46a
Aleksa Djordjevic <aleksadjordjevicts@gmail.com>
2025-06-19 13:23:38 +0200

    Crud aplikacija #8

---
+++ sandbox/Aleksa_Djordjevic/crud-app/front/VideoGamesFront/.angular/cache/20.0.2/VideoGamesFront/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-OELFBZ3W.js";
+import "./chunk-Q5EV5Q7F.js";
+import "./chunk-TV67TKZY.js";
+import "./chunk-PVHDC332.js";
+import "./chunk-QSBBMNCS.js";
+import "./chunk-VHYEF4KI.js";
+import "./chunk-VZADVIZC.js";
+import "./chunk-2XLRDDJW.js";
+import "./chunk-43KPLV43.js";
+import "./chunk-TXGYY7YM.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
