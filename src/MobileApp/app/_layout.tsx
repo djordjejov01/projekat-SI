8e1b4d32e58c279a5cf6f691a880a4c2805aad51
Jovana Sajkic <sajkicj2003@gmail.com>
2025-06-23 19:20:47 +0200

    Kreirana forma za registraciju i prijavu #28

--- src/MobileApp/app/_layout.tsx
+++ src/MobileApp/app/_layout.tsx
@@ -1,33 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-
+import { useFonts } from 'expo-font';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    'JustMeAgain': require('../assets/fonts/JustMeAgainDownHere.ttf'),
-  });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  // const [loaded] = useFonts({
+  //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  //   'JustMeAgain': require('../assets/fonts/JustMeAgainDownHere.ttf'),
+  // });
+
+  // if (!loaded) return null;
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
-
-
