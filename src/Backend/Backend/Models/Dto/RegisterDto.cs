31b290b82734a1149003b9ed7835c48eda09910d
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-06-24 14:19:36 +0200

    Kreiran RegisterDto za prenos podataka pri registraciji

---
+++ src/Backend/Backend/Models/Dto/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string? Role { get; set; }
+    }
+}
