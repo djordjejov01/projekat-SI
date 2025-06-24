f9650c17ecb7473cc46dd0c184fb7cf87148d839
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-06-24 16:09:14 +0200

    Kreiran AppDbContext sa DbSet<User> za povezivanje sa bazom

---
+++ src/Backend/Backend/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        // ovde dodajem i ostale Dbset(Events...)
+    }
+} 
