81f6324af9bd86b718500472a905f960057893b4
mihailo031 <bolovicmihailo@gmail.com>
2025-06-26 10:26:19 +0200

    add models, update dbContect

--- src/Backend/Backend/Models/AppDbContext.cs
+++ src/Backend/Backend/Models/AppDbContext.cs
@@ -10,6 +10,12 @@ namespace Backend.Models
 
         public DbSet<User> Users { get; set; }
         public DbSet<UserRoles> UserRoles {  get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventActivity> EventActivities { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<EventResource> EventResources { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<UserTicket> UserTickets { get; set; }
 
         // ovde dodajem i ostale Dbset(Events...)
     }
