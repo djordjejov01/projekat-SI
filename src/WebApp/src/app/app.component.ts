0081e60e2643a74e062a184e5cb19ad1debd25c8
Stefan Grujicic <stefan7822@gmail.com>
2025-07-01 01:25:55 +0200

    Implementirana lokalizacija za engleski i srpski, instalirani paketi ngx translate i i18n, konfigurisani importi, napravljene funkcije i funkcionalnost promene sa jednog na drugi jezik, napravljeni fajlovi za engleski i srpski sa 2 test primera #32

--- src/WebApp/src/app/app.component.ts
+++ src/WebApp/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService, TranslateModule, TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'WebApp';
+   constructor(private translate: TranslateService) {
+    translate.addLangs(['sr', 'en']);
+    translate.setDefaultLang('sr');
+    translate.use('sr');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
--- src/WebApp/src/app/app.config.ts
+++ src/WebApp/src/app/app.config.ts
@@ -4,13 +4,35 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { MyPreset } from './myPreset';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateLoader, provideTranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {importProvidersFrom } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'sr',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimationsAsync(),
