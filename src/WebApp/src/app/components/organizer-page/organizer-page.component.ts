05e02286ac5bb512c3dd00ae46eb1602911d3124
Stefan Grujicic <stefan7822@gmail.com>
2025-07-02 12:19:14 +0200

    Dodate informativne kartice sa statistikom #33

--- src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
+++ src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { EventsComponent } from './events/events.component';
 import { OverviewComponent } from './overview/overview.component';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-organizer-page',
-  imports: [MenuBarComponent, EventsComponent, OverviewComponent],
+  imports: [MenuBarComponent, EventsComponent, OverviewComponent,RouterModule],
   templateUrl: './organizer-page.component.html',
   styleUrl: './organizer-page.component.css'
 })
