19b78f2e4a877892e52989b231fab013191dea1d
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-07-02 16:32:07 +0200

    Dodat Dto EventDetails

---
+++ src/Backend/Backend/Models/Dto/EventDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend.Models.Dto
+{
+    public class EventDetailsDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public int OrganizerId { get; set; }
+        public string OrganizerName { get; set; }
+        public int AttendingCount { get; set; }
+        public bool IsFavorite { get; set; }
+        public List<AgendaItemDto> Agenda { get; set; }
+    }
+}
