35c2af90ba5f2d94bc07833665d9db9e488fb032
Stefan Grujicic <stefan7822@gmail.com>
2025-07-04 00:13:35 +0200

    Dodata lokalizacija na stranici organizatora za staticne elemente #32

--- src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
+++ src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
@@ -4,12 +4,21 @@ import { EventsComponent } from './events/events.component';
 import { OverviewComponent } from './overview/overview.component';
 import { RouterModule } from '@angular/router';
 import { FooterBar } from '../landing-page/footer-bar/footer-bar';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-organizer-page',
-  imports: [MenuBarComponent, EventsComponent, OverviewComponent,RouterModule, FooterBar],
+  imports: [MenuBarComponent, OverviewComponent,RouterModule, FooterBar, TranslateModule],
   templateUrl: './organizer-page.component.html',
   styleUrl: './organizer-page.component.css'
 })
 export class OrganizerPageComponent {
 
+  constructor(private translate: TranslateService) {
+    }
+  changeLanguage(event: Event) {
+  const selectElement = event.target as HTMLSelectElement;
+  const lang = selectElement.value;
+  this.translate.use(lang);
+}
 }
