5e57f9103be08ea7bbad18e5b38b94247622f29a
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-07-08 12:10:38 +0200

    Dodata polja za tip karte, kvote, opis

--- src/Backend/Backend/Models/Ticket.cs
+++ src/Backend/Backend/Models/Ticket.cs
@@ -10,9 +10,13 @@ namespace Backend.Models
 
         [ForeignKey(nameof(Event))]
         public int EventID { get; set; }
-
-        public int Price { get; set; }
-        public int Quantity { get; set; }
+        [Required,MaxLength(50)]
+        public string TypeName {  get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Quota { get; set; }
 
         public Event Event { get; set; }
     }
