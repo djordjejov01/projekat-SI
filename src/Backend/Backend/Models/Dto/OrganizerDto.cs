de5f17c8fff82318c5ed9ce77d84a8281cd5e9d3
mihailo031 <bolovicmihailo@gmail.com>
2025-07-08 13:41:03 +0200

    add neccessary tables and models

---
+++ src/Backend/Backend/Models/Dto/OrganizerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models.Dto
+{
+    public class OrganizerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AdminAddress { get; set; }
+    }
+}
