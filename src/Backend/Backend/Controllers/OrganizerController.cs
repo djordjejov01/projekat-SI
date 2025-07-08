5245d463b2726dc088afc8a39d5a80d933598e0f
mihailo031 <bolovicmihailo@gmail.com>
2025-07-08 13:47:42 +0200

    add basic endpoint, fix organizerdto

---
+++ src/Backend/Backend/Controllers/OrganizerController.cs
@@ -0,0 +1,36 @@
+﻿using Backend.Models;
+using Backend.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Organizer")]
+
+    public class OrganizerController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public OrganizerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("organizer")]
+        public async Task<IActionResult> GetOrganizer(int id)
+        {
+            var Organizer = _context.Organizers.Where(o => o.Id == id).Select(o => new OrganizerDto
+            {
+                Id = o.Id,
+                Username = o.Username,
+                Email = o.Email,
+                Name = o.Name,
+                PhoneNumber = o.PhoneNumber
+            }).FirstOrDefault();
+            if(Organizer is not null)
+                return Ok(Organizer);
+            return BadRequest(new { message = "Organizer with that ID does not exist." });
+        }
+    }
+}
--- src/Backend/Backend/Models/Dto/OrganizerDto.cs
+++ src/Backend/Backend/Models/Dto/OrganizerDto.cs
@@ -7,6 +7,5 @@
         public string Username { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string AdminAddress { get; set; }
     }
 }
