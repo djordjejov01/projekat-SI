e5aebb2654f378f4d203a138188fdccac01b01d8
Stefan Grujicic <stefan7822@gmail.com>
2025-07-17 02:29:28 +0200

    Prikaz predstojecih dogadjaja ulogovanog organizatora povezano sa backendom kada se udje na dashboard, priakzuju se svi podaci o svakom dogadjaju i pakuju u listu #57

--- src/WebApp/src/app/components/organizer-page/overview/overview.component.ts
+++ src/WebApp/src/app/components/organizer-page/overview/overview.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewestEventComponent } from './newest-event/newest-event.component';
 import { EventPreviewComponent } from './event-preview/event-preview.component';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { Event } from '../../../Models/Event';
+import { ApiService } from '../../../Services/api.service';
+import { AuthService } from '../../../Services/auth.service';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-overview',
-  imports: [NewestEventComponent, EventPreviewComponent, TranslateModule],
+  imports: [NewestEventComponent, EventPreviewComponent, TranslateModule, CommonModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+
+
+  constructor(private apiService : ApiService, private authService : AuthService, private messageService : MessageService){}
+  allEvents : Event[];
+  currUser : string;
+  ngOnInit(): void {
+      this.currUser = this.authService.getUserName();
+      this.apiService.getOrganizerEvents(this.authService.getUserId()).subscribe({
+      
+              next:(response : Event[]) => {
+                this.allEvents = response;
+                console.log(response);
+              },
+              error:(errorResponse) =>{
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: errorResponse.message,
+                    life: 3000 });
+              }
+      
+            })
+      
+      
+  }
 }
