b39bfbc0b9f6d38f57288414c36b7c81a41be42b
Stefan Grujicic <stefan7822@gmail.com>
2025-07-19 20:58:23 +0200

    Kreirana tabela povezana sa backendom za prikaz podataka o dogadjajima u tabeli #63

--- src/WebApp/src/app/components/organizer-page/events/events.component.ts
+++ src/WebApp/src/app/components/organizer-page/events/events.component.ts
@@ -1,11 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Tag } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { InputIcon } from 'primeng/inputicon';
+import { IconField } from 'primeng/iconfield';
+import { CommonModule } from '@angular/common';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { Slider } from 'primeng/slider';
+import { ProgressBar } from 'primeng/progressbar';
+import { ApiService } from '../../../Services/api.service';
+import { AuthService } from '../../../Services/auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { Event } from '../../../Models/Event';
+import { AfterContentInit, AfterViewInit, HostListener, ViewChild } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, inject, PLATFORM_ID } from '@angular/core';
+import { UIChart } from 'primeng/chart'
+import { TableLazyLoadEvent } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import { Table } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { Toast } from 'primeng/toast';
+import { RouterLink } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [CommonModule,ChartModule,TableModule, ButtonModule,
+      CommonModule, MultiSelectModule, InputTextModule, DropdownModule, FormsModule,IconField, InputIcon,TableModule,ConfirmDialogModule],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
 
+    statuses!: any[];
+
+    loading: boolean = true;
+
+    activityValues: number[] = [0, 100];
+    allEvents : Event[];
+    selectedEvents: Event[];
+    searchValue : string;
+      currUser : string;
+    constructor(private apiService : ApiService, private authService : AuthService, private messageService : MessageService,
+    private router : Router) {}
+    clear(table: Table) {
+        table.clear();
+        this.selectedEvents = [];
+        this.searchValue = "";
+      }  
+    value: any[] = [];
+    roles = [
+  { name: 'Admin', value: 'Admin'},
+  { name: 'Organizer', value: 'Organizer' },
+  { name: 'Supplier', value: 'Supplier' },
+  // Add all roles you have
+];
+    viewEvent(eID : number)
+    {
+      alert(eID);
+    }
+    editEvent(eID : number)
+    {
+      alert(eID);
+    }
+    deleteEvent(eID : number)
+    {
+      alert(eID);
+    }
+    ngOnInit() {
+        this.currUser = this.authService.getUserName();
+              this.apiService.getOrganizerEvents(this.authService.getUserId()).subscribe({
+              
+                      next:(response : Event[]) => {
+                        this.allEvents = response;
+                        this.loading = false;
+                        console.log(response);
+                      },
+                      error:(errorResponse) =>{
+                        this.messageService.add({
+                            severity: 'error',
+                            summary: 'Error',
+                            detail: errorResponse.message,
+                            life: 3000 });
+                      }
+              
+                    })
+
+        this.statuses = [
+            { label: 'Unqualified', value: 'unqualified' },
+            { label: 'Qualified', value: 'qualified' },
+            { label: 'New', value: 'new' },
+            { label: 'Negotiation', value: 'negotiation' },
+            { label: 'Renewal', value: 'renewal' },
+            { label: 'Proposal', value: 'proposal' }
+        ];
+    }
+
+    getSeverity(status: string) {
+        switch (status) {
+            case 'unqualified':
+                return 'danger';
+
+            case 'qualified':
+                return 'success';
+
+            case 'new':
+                return 'info';
+
+            case 'negotiation':
+                return 'warn';
+
+            case 'renewal':
+                return null;
+            default:
+              return null;
+        }
+    }
 }
