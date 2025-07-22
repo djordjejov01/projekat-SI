da74138b0f7088ebede2ef9c09a7cc43a6cb2895
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-07-22 15:08:28 +0200

    Dodat get za stanje na racunu

---
+++ src/Backend/Backend/Controllers/CreditController.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CreditController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserCredit()
+        {
+            var userId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value);
+
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+                return NotFound();
+            return Ok(user.Credit);
+        }
+    }
+}
