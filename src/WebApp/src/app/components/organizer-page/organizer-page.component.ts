23121c23e9bb936a90bb91ab164528cad47b67d3
Stefan Grujicic <stefan7822@gmail.com>
2025-07-25 15:35:29 +0200

    Dodata funkcija kada se klikne na profil picture da vodi na my-profile, kada se azurira slika u my-profile azurira se automatski i desno gore preko shared service-a, omoguceno da se edituje newest event na vrhu overview stranice, dodato par animacija

--- src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
+++ src/WebApp/src/app/components/organizer-page/organizer-page.component.ts
@@ -13,49 +13,65 @@ import { Toast } from 'primeng/toast';
 import { CategoryService } from '../../Services/EventCategoryService';
 import { ApiService } from '../../Services/api.service';
 import { OrganizerDto } from '../../Models/OrganizerDto';
-
+import { SharedService } from '../../Services/shared.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-organizer-page',
-  imports: [MenuBarComponent,RouterModule,ConfirmDialogModule,ToastModule,TranslateModule,Toast],
+  imports: [MenuBarComponent, RouterModule, ConfirmDialogModule, ToastModule, TranslateModule, Toast],
   templateUrl: './organizer-page.component.html',
   styleUrl: './organizer-page.component.css'
 })
-export class OrganizerPageComponent implements AfterContentInit,OnInit{
+export class OrganizerPageComponent implements AfterContentInit, OnInit {
 
   constructor(
-    private sessionService : SessionService,
+    private sessionService: SessionService,
     private translate: TranslateService,
-    private messageService : MessageService,
-    private authService : AuthService,
-    private cd : ChangeDetectorRef,
-    private categoryService : CategoryService,
-    private apiService : ApiService){}
+    private messageService: MessageService,
+    private authService: AuthService,
+    private cd: ChangeDetectorRef,
+    private categoryService: CategoryService,
+    private apiService: ApiService,
+    private sharedService: SharedService,
+    private router : Router) { }
 
-    currOrganizer : OrganizerDto;
-    defaultImage = 'assets/default-picture.png';
-    previewUrl: string | ArrayBuffer | null = null;
-    ngOnInit(): void {
-      this.categoryService.loadCategories().subscribe();
+  currOrganizer: OrganizerDto;
+  defaultImage = 'assets/default-picture.png';
+  previewUrl: string | ArrayBuffer | null = null;
 
-      this.apiService.getOrganizer(this.authService.getUserId()).subscribe({
-      
-              next:(response : OrganizerDto) => {
-                this.currOrganizer = response;
-                if(this.currOrganizer.getImage()!="https://localhost:7269/")
-                {
-                  this.previewUrl = this.currOrganizer.getImage();
-                }
-              },
-              error:(errorResponse) =>{
-                this.messageService.add({
-                    severity: 'error',
-                    summary: 'Error',
-                    detail: errorResponse.message,
-                    life: 3000 });
-              }
-      
-            })
-    }
+  getOrganizerCall() {
+    this.apiService.getOrganizer(this.authService.getUserId()).subscribe({
+
+      next: (response: OrganizerDto) => {
+        this.currOrganizer = response;
+        if (this.currOrganizer.getImage() != "https://localhost:7269/") {
+          this.previewUrl = this.currOrganizer.getImage();
+        }
+      },
+      error: (errorResponse) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: errorResponse.message,
+          life: 3000
+        });
+      }
+
+    })
+  }
+
+
+
+  ngOnInit(): void {
+    this.sharedService.profileImageChanged$.subscribe(changed => {
+      if (changed) {
+        this.getOrganizerCall(); // metoda koja uzima novu sliku iz baze/backenda
+      }
+    });
+
+    this.categoryService.loadCategories().subscribe();
+
+    this.getOrganizerCall();
+  }
 
   ngAfterContentInit(): void {
     this.cd.detectChanges(); // Ensure view is fully initialized
@@ -76,13 +92,20 @@ export class OrganizerPageComponent implements AfterContentInit,OnInit{
   }
 
 
-    onLogoutClick(){
+  onLogoutClick() {
     this.sessionService.logoutWithConfirmation();
   }
 
-    changeLanguage(event: Event) {
+  changeLanguage(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const lang = selectElement.value;
     this.translate.use(lang);
+  }
+
+  myProfile(){
+    this.router.navigate(["/organizer/my-profile"],{
+        queryParams: { showID: 4}
+      });
+
   }
 }
