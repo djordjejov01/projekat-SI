7a6188b805f5a445f6fff633c197e53ed6083ca1
Nikola Jevtic <61-2022@pmf.kg.ac.rs>
2025-07-27 18:31:42 +0200

    Napravljena modalna forma za dodavanje ulaznica

---
+++ src/WebApp/src/app/components/organizer-page/event-management/ticket-section/ticket-modal/ticket-modal.component.ts
@@ -0,0 +1,103 @@
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { EventBasicInfo } from '../../../../../Models/EventBasicInfo';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomValidators } from '../../../../../Validators/custom.validators';
+import { MessageService } from 'primeng/api';
+import { FormValidationService } from '../../../../../Services/FormValidationService';
+import { DialogModule } from 'primeng/dialog';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputNumber } from 'primeng/inputnumber';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { TranslateService } from '@ngx-translate/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { ButtonModule } from 'primeng/button';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-ticket-modal',
+  imports: [ReactiveFormsModule,DialogModule,DatePickerModule,InputNumber,FloatLabelModule,InputTextModule,TextareaModule,ButtonModule],
+  templateUrl: './ticket-modal.component.html',
+  styleUrl: './ticket-modal.component.css'
+})
+export class TicketModalComponent implements OnInit,OnChanges, OnDestroy{
+
+
+  @Input() eventBasicInfo : EventBasicInfo;
+  ticketForm : FormGroup;
+  visible : boolean = false;
+
+  currencyCode : string;
+  localeCode : string;
+  private langChangeSub: Subscription;
+
+  constructor(private messageService : MessageService, private formValidationService : FormValidationService, private translateService : TranslateService) {}
+
+  ngOnInit(): void {
+
+    this.initializeForm()
+
+    const currentLang = this.translateService.currentLang || 'en';
+    this.setLocalFormLang(currentLang);
+
+       this.langChangeSub = this.translateService.onLangChange.subscribe(lang => {
+        this.setLocalFormLang(lang.lang)
+    });
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['eventBasicInfo' ] && this.eventBasicInfo){
+      this.ticketForm.reset()
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this.langChangeSub?.unsubscribe();
+  }
+
+  show(){
+    this.visible = true;
+  }
+
+  hide() {
+    this.visible = false;
+    this.ticketForm.reset();
+  }
+
+  initializeForm(){
+
+     this.ticketForm = new FormGroup({
+      name: new FormControl('',[Validators.required, CustomValidators.noWhitespaceValidator]),
+      price: new FormControl('', [Validators.required, Validators.min(0)]),
+      description: new FormControl('', CustomValidators.noWhitespaceValidator),
+      quota: new FormControl('',[Validators.required,Validators.min(1)]),
+      validFrom: new FormControl('', Validators.required),
+      validUntil: new FormControl('', Validators.required)
+
+    }, {validators: CustomValidators.startBeforeEndDates('validFrom','validUntil')})
+
+  }
+
+  private setLocalFormLang(lang : string){
+      if (lang === 'sr') {
+        this.currencyCode = 'RSD';
+        this.localeCode = 'sr-RS';
+      } else {
+        this.currencyCode = 'EUR';
+        this.localeCode = 'en-US';
+      }
+  }
+
+
+  submitForm(){
+    if(this.ticketForm.invalid){
+      this.formValidationService.showValidationErrors(this.ticketForm,'Ticket');
+      return;
+    }
+
+    console.log(this.ticketForm.value)
+  }
+
+}
--- src/WebApp/src/app/components/organizer-page/event-management/ticket-section/ticket-section.component.ts
+++ src/WebApp/src/app/components/organizer-page/event-management/ticket-section/ticket-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MOCK_TICKETS, Ticket } from '../../../../MockData/MockTickets';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
@@ -11,15 +11,19 @@ import { IconField } from 'primeng/iconfield';
 import { Table } from 'primeng/table';
 import { DatePipe } from '@angular/common';
 import { TooltipModule } from 'primeng/tooltip';
+import { EventBasicInfo } from '../../../../Models/EventBasicInfo';
+import { TicketModalComponent } from './ticket-modal/ticket-modal.component';
 
 @Component({
   selector: 'app-ticket-section',
-  imports: [TableModule,ButtonModule,MultiSelectModule,InputTextModule,DropdownModule,FormsModule,InputIcon,IconField,TooltipModule, DatePipe],
+  imports: [TableModule,ButtonModule,MultiSelectModule,InputTextModule,DropdownModule,FormsModule,InputIcon,IconField,TooltipModule, DatePipe,TicketModalComponent],
   templateUrl: './ticket-section.component.html',
   styleUrl: './ticket-section.component.css'
 })
 export class TicketSectionComponent {
 
+
+  @Input() eventBasicInfo : EventBasicInfo;
 
   tickets : Ticket[] = MOCK_TICKETS;
   selectedTickets : Ticket [] = [];
