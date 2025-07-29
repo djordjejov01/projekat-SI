b2933ac40620c85392a160d4a52d1db6b2735934
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-07-29 13:08:34 +0200

    Dodat get supplier

---
+++ src/Backend/Backend/Controllers/SupplierController.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Models;
+using Backend.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Supplier")]
+    public class SupplierController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SupplierController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetSupplier()
+        {
+            var userId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value);
+
+            var supplier = await _context.Suppliers
+                .FirstOrDefaultAsync(s => s.Id == userId);
+
+            if (supplier == null)
+                return NotFound("Dobavljač nije pronađen.");
+
+            var dto = new SupplierDto
+            {
+                Id = supplier.Id,
+                Username=supplier.Username,
+                CompanyName = supplier.CompanyName,
+                Email = supplier.Email,
+                PhoneNumber = supplier.PhoneNumber,
+                Website = supplier.Website,
+                CompanyBio = supplier.CompanyBio,
+                Image = supplier.Image
+            };
+
+            return Ok(dto);
+        }
+    }
+}
