0945c33f9dca3955b194f58c0a788309c9d4edcb
Djordje Jovanovic <djordjejovanovic037@gmail.com>
2025-08-05 22:03:31 +0200

    Dodato dohvatanje resursa za dobavljaca #83

--- src/Backend/Backend/Controllers/SupplierController.cs
+++ src/Backend/Backend/Controllers/SupplierController.cs
@@ -123,19 +123,19 @@ namespace Backend.Controllers
             return Ok();
         }
 
-        [HttpGet("Resources/{supplierId}")]
-        public async Task<IActionResult> GetSupplierResources(int supplierId)
+        [HttpGet("Resources")]
+        public async Task<IActionResult> GetSupplierResources()
         {
 
             var supplierId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value);
-            // Validacija da li dobavljač postoji
+            
             var supplier = await _context.Suppliers
                 .FirstOrDefaultAsync(s => s.Id == supplierId);
 
             if (supplier == null)
                 return NotFound("Dobavljač nije pronađen.");
 
-            // Dohvatanje svih resursa za dobavljača
+            
             var resources = await _context.Resources
                 .Where(r => r.SupplierID == supplierId)
                 .Select(r => new
