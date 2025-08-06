9234034c16947deb230cc21ef3fb24aa74ac4d45
mihailo031 <bolovicmihailo@gmail.com>
2025-08-06 20:44:55 +0200

    add missing migration

---
+++ src/Backend/Backend/Migrations/20250806184340_AlterAvailbailityResource.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterAvailbailityResource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IsAvailable",
+                table: "Resources",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Measurment",
+                table: "Resources",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Measurment",
+                table: "Resources");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAvailable",
+                table: "Resources",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
