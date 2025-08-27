bdc32feecce3a45b20760b7a1b5b8a49153ae7ce
Aleksa Djordevic <aleksadjordjevicts@gmail.com>
2025-08-27 15:23:39 +0200

    Dodato sortiranje karata po datumu i vremenu kupovine #113

--- src/MobileApp/app/profile/myTickets.tsx
+++ src/MobileApp/app/profile/myTickets.tsx
@@ -81,7 +81,17 @@ export default function ProfileTickets() {
             }
           });
 
-          setGroupedTickets(Object.values(grouped));
+          const groupedArray = Object.values(grouped);
+
+
+        groupedArray.sort((a, b) => {
+          const dateA = new Date(a.purchasedAt).getTime();
+          const dateB = new Date(b.purchasedAt).getTime();
+          return dateB - dateA; // rastući redosled (najstarija kupovina prva)
+        });
+
+        setGroupedTickets(groupedArray);
+
         } else {
           console.warn('Failed to fetch tickets');
         }
