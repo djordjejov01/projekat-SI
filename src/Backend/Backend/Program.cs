bb213cb51424360f41d9034c3e18b3861177d208
Stefan Grujicic <stefan7822@gmail.com>
2025-09-08 20:55:17 +0200

    Izmenjeni linkovi za fail i success i dodato using backend u program.cs

--- src/Backend/Backend/Program.cs
+++ src/Backend/Backend/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using Backend.Models.Dto;
+using Backend;
 
 
 var builder = WebApplication.CreateBuilder(args);
