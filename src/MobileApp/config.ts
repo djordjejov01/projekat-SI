4556d426efd249c2954cd187eb2666ebadd430f8
Jovana Sajkic <sajkicj2003@gmail.com>
2025-09-10 10:57:13 +0200

    config

--- src/MobileApp/config.ts
+++ src/MobileApp/config.ts
@@ -1,10 +1,10 @@
 // config.ts
-export const API_URL = "http://192.168.47.32:11061";
+// export const API_URL = "http://192.168.47.32:11061";
 // config.ts
 
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// export const API_URL = "http://softeng.pmf.kg.ac.rs:11061";
+export const API_URL = "http://softeng.pmf.kg.ac.rs:11061";
 export const apiCall = async (url: string, options: RequestInit = {}) => {
   const lang = await AsyncStorage.getItem('lang') || 'sr'; // default sr
   return fetch(url, {
